4bd5c3ae58d7333c98e746d04f57560dfe45dabf
Siddhartha Shrestha <sdhrtivv@gmail.com>
2025-10-22 09:52:12 +0545

    turned off signcolumn

--- init.lua
+++ init.lua
@@ -39,7 +39,7 @@ vim.o.smartcase = true
 vim.o.hlsearch = true
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = 'no'
 
 -- Decrease update time
 vim.o.updatetime = 250
