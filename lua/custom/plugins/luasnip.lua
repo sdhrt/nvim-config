bdcc215b9ea2de125e07f1b280d5dba0b3032a8b
Siddhartha Shrestha <sdhrtivv@gmail.com>
2025-10-28 14:42:59 +0545

    better luasnip config

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -1,12 +1,10 @@
 return {
-  'L3MON4D3/LuaSnip',
-  dependencies = {
-    'rafamadriz/friendly-snippets',
-  },
-  version = 'v2.*',
-  config = function()
-    local ls = require 'luasnip'
-    ls.setup { enable_autosnippets = true }
-    require('luasnip.loaders.from_vscode').lazy_load()
-  end,
+    'L3MON4D3/LuaSnip',
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+    },
+    version = 'v2.*',
+    config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+    end,
 }
